81c29e2e9427970afc984a9337681c1d4bcb5ab6
Jaeha Lee <wogkdkrm112@gmail.com>
2024-09-27 20:12:30 +0900

    init: init project

---
+++ trafficone/src/main/java/com/jet/be/TrafficoneApplication.java
@@ -0,0 +1,13 @@
+package com.jet.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficoneApplication.class, args);
+    }
+
+}
---
+++ trafficone/src/test/java/com/jet/be/TrafficoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jet.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
