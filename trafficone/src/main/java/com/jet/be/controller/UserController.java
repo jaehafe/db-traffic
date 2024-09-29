43dedf2318410d3c976844b91b2db0a6f74a3eed
Jaeha Lee <wogkdkrm112@gmail.com>
2024-09-29 22:06:48 +0900

    feat: 회원탈퇴 기능

--- trafficone/src/main/java/com/jet/be/controller/UserController.java
+++ trafficone/src/main/java/com/jet/be/controller/UserController.java
@@ -3,6 +3,8 @@ package com.jet.be.controller;
 import com.jet.be.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,12 @@ public class UserController {
     @PostMapping("/create")
     public ResponseEntity<Void> createUser(String username, String password, String email) {
         userService.createUser(username, password, email);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
         return ResponseEntity.ok().build();
     }
 }
